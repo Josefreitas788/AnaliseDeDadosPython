5ad8da091c652232dce8c9bc379dfde50001f56a
josefreitas788 <josefreitas788@gmail.com>
2022-10-11 13:09:14 -0300

    criação do grafico de quantidade de beneficiarios por região

--- analise_AuxilioBrasil/analise_AuxilioBrasil.py
+++ analise_AuxilioBrasil/analise_AuxilioBrasil.py
@@ -1,9 +1,7 @@
 import pandas as pd 
-import matplotlib.pyplot as plt
 import seaborn as sns
 
-
-chunk_pd = pd.read_csv('202207_AuxilioBrasil.csv',encoding = 'iso-8859-15',sep=';', chunksize=100000)
+chunk_pd = pd.read_csv('~/Downloads/202207_AuxilioBrasil.csv',encoding = 'iso-8859-15',sep=';', chunksize=100000)
 listdf = []
 regioes ={
     'norte' : ['AC','AM','AP','PA','RO','RR','TO'],
@@ -12,29 +10,34 @@ regioes ={
     'sul' : ['PR','RS','SC'],
     'centro_oeste' : ['DF','GO','MT','MS']
 }
-
+cont = 0
 df = pd.DataFrame()
 for piece in chunk_pd:
-
+    
     piece = piece.drop(['MÊS COMPETÊNCIA','NOME FAVORECIDO','NIS FAVORECIDO','CÓDIGO MUNICÍPIO SIAFI', 'CPF FAVORECIDO'],axis=1)
     piece['regiao'] = piece['UF'].apply(lambda uf: 'norte' if uf in regioes['norte'] else 'nordeste' if uf in regioes['nordeste'] else 'sudeste' if uf in regioes['sudeste'] else 'sul' if uf in regioes['sul'] else 'centro_oeste')
-    df = piece
-    break
+    #df = piece
+    #break
     listdf.append(piece)
+    # cont =+ 1
+    # if cont == 2:
+    #     break
     
 
-#df = pd.concat(listdf)
+df = pd.concat(listdf)
 
 #Não há valores varios no dataframe
 #print(df["VALOR PARCELA"].sum())
 
 print(df)
+sns.set(style="whitegrid")
+ax = sns.countplot(x="regiao", data=df)
+ax.set_title('Auxilio Brasil por Região')
+ax.set_xlabel('Região')
+ax.set_ylabel('Quantidade de Beneficiários')
+ax.figure.savefig('auxilio_brasil_regiao.png')
 
-sns.set(style="darkgrid")
-
-plt.show()
-
-
+ax = sns.countplot(x="", data=df)
 
     
 
