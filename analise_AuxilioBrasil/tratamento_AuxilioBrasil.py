6f0650ae1aee270d7eeb8a2423812a4d50aadfe8
josefreitas788 <josefreitas788@gmail.com>
2022-10-18 08:50:46 -0300

    separando codigo do tratamento dos dados do auxlio

---
+++ analise_AuxilioBrasil/tratamento_AuxilioBrasil.py
@@ -0,0 +1,26 @@
+import pandas as pd 
+import seaborn as sns
+
+chunk_pd = pd.read_csv('~/Downloads/202207_AuxilioBrasil.csv',encoding = 'iso-8859-15',sep=';', chunksize=100000)
+listdf = []
+regioes ={
+    'norte' : ['AC','AM','AP','PA','RO','RR','TO'],
+    'nordeste' : ['AL','BA','CE','MA','PB','PE','PI','RN','SE'],
+    'sudeste' : ['ES','MG','RJ','SP'],
+    'sul' : ['PR','RS','SC'],
+    'centro_oeste' : ['DF','GO','MT','MS']
+}
+cont = 0
+df = pd.DataFrame()
+for piece in chunk_pd:
+    
+    piece = piece.drop(['MÊS COMPETÊNCIA','NOME FAVORECIDO','NIS FAVORECIDO','CÓDIGO MUNICÍPIO SIAFI', 'CPF FAVORECIDO'],axis=1)
+    piece['regiao'] = piece['UF'].apply(lambda uf: 'norte' if uf in regioes['norte'] else 'nordeste' if uf in regioes['nordeste'] else 'sudeste' if uf in regioes['sudeste'] else 'sul' if uf in regioes['sul'] else 'centro_oeste')
+    listdf.append(piece)
+
+    
+
+df = pd.concat(listdf)
+df.to_csv('auxilioBrasil_202207.csv')
+
+
